f6fd41e0a9727d1e26fc4cad7205e67157c82c4a
Josh Starcher <josh.starcher@gmail.com>
2012-05-28 11:50:19 -0400

    some code tips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
   protect_from_forgery
 
+  # You don't need these render methods. You can just use respond_to and respond_with to do the same thing. 
+  # Look at this article: http://davidwparker.com/2010/03/09/api-in-rails-respond-to-and-respond-with/
   def renderJS
   	respond_to do |format|
     	format.js if request.xhr?
@@ -16,8 +18,14 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  # finding a contact should always be scoped to user for security reasons. If you call Contact.find(id), it is easy for me to 
+  # hack your app by sending in an id of a contact i don't own. Instead use current_user.contacts.find(id)
+  # This comment is true in your other controllers as well. Everywhere you use Contact, replace it with current_user.contacts
+  #
+  # Really i don't see the reason for this method. It doesn't save any typing.
   def get_contact_from_id(id)
     Contact.find(id)
+    # current_user.contacts.find(id)
   end
 
   protected
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,13 @@
 class ContactsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user! # move this to ApplicationController so you don't have to remeber to put it in each controller
+
   # GET /contacts
   # GET /contacts.json
   def index
-    
     session[:order] = "first_name" unless session[:order].present?
     @order = session[:order]
+    #@order = session[:order] ||= 'first_name'
+
     session[:conditions] = "user_id = #{current_user.id} AND status <> 'deleted'" unless session[:conditions].present?
     @conditions = session[:conditions]
     @contacts = Contact.where(@conditions).order(@order)
@@ -45,22 +47,37 @@ class ContactsController < ApplicationController
   # POST /contacts
   # POST /contacts.json
   def create
-    params[:contact][:user_id] = current_user.id
+
+    #################
+    #  replace this #
+    params[:contact][:user_id] = current_user.id # this isn't needed if you use current_user.contacts.create
     params[:contact][:source] = "Noticery"
     params[:contact][:source_reference] = ""
     @contact = Contact.create(params[:contact])
+
+    # with this    #
+    @contact = current_user.contacts.create(params[:contact].merge(source: 'Noticery', source_reference: ''))
+    ################
+
     @notice = 'Contact was successfully created'
     
+    #################
+    #  replace this #
     @add_another = params[:extra_add_another]
     if @add_another.to_i == 1
       @contact = nil
       @contact = Contact.new 
     end
-    
+
+    # with this    #
+    @contact = Contact.new if params[:extra_add_another] == '1'
+    ################
+
+
     @order = session[:order]
     @conditions = session[:conditions]
     @contacts = Contact.where(@conditions).order(@order)
-    
+
     renderJS
   end
 
@@ -68,13 +85,25 @@ class ContactsController < ApplicationController
   # PUT /contacts/1.json
   def update
     @contact = Contact.find(params[:id])
+    # your need to check and see if the update worked
+    # if @contact.update_attributes(params[:contact])
     @contact.update_attributes(params[:contact])
-    @notice = 'Contact was successfully updated'
+
+      @notice = 'Contact was successfully updated'
+    # else
+      # error message, record not valid
+    # end
     
+    #################
+    #  replace this #
     if @contact.status == 'imported'
       @contact.status = 'active'
       @contact.save
     end
+    # with this    #
+    @contact.update_attribute(:status, 'active') if @contact.status == 'imported'
+    ################
+
     
     @add_another = params[:extra_add_another]
     
@@ -92,10 +121,15 @@ class ContactsController < ApplicationController
 
   def confirmed_delete
     @ids_raw = params[:confirm_delete][:ids]
-    
+
+    #################
+    #  replace this #
     sql = ActiveRecord::Base.connection()
     sql.execute("UPDATE contacts SET status = 'deleted' WHERE id IN (#{@ids_raw})")
-    
+    # with this    #
+    Contact.where(id: @ids_raw).update_all("status = 'deleted'") # http://api.rubyonrails.org/classes/ActiveRecord/Relation.html#method-i-update_all
+    ################
+
     @notice = "Selected contacts successfully deleted"
     @order = session[:order]
     @conditions = session[:conditions]
