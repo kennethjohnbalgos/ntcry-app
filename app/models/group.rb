94def431ae5506c31908ae18a41a71e50c428ea6
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-05-29 16:21:36 +0800

    Implement adding of groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ActiveRecord::Base
   attr_accessible :description, :name, :position, :user_access, :user_id
   
   validates_presence_of :name, :description
+  validates_length_of :name, :maximum => 15
+  validates_length_of :description, :maximum => 150
   
   belongs_to :user
   has_many :group_contacts
